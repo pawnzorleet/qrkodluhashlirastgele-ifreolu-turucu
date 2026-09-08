3dd3b7b441fc38c3d6197cb83f0ef81e3d4a9129
pawnzorleet <stoneguruduo@gmail.com>
2026-09-08 11:29:06 +0300

    qrhashsifreolusturucu.py

--- qrhashsifreolusturucu.py
+++ qrhashsifreolusturucu.py
@@ -29,7 +29,7 @@ st.button('Yenile')
 st.write('Şifre: ',''.join(sifre3))
 st.write('Hash Şifre: ',hash_sifre)
 
-st.image('https://m.media-amazon.com/images/I/61h7XvsUBRL._AC_SL1500_.jpg')
+st.image('https://scontent.fsaw1-11.fna.fbcdn.net/v/t1.6435-9/118794700_3090240834435213_1281607157223323725_n.jpg?stp=dst-jpg_tt6&cstp=mx675x379&ctp=s675x379&_nc_cat=104&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=127cfc&_nc_ohc=ujGjM3y7n6AQ7kNvwHNecuU&_nc_oc=AdrZ_FdIIVX96hcdNLIfIpb-RRT30COsmBlwc0AasXfuFHljqwFLU77AMzJwvIaL3to&_nc_zt=23&_nc_ht=scontent.fsaw1-11.fna&_nc_gid=PNc7Stzhn_WoVaYWg95uvw&_nc_ss=7b289&oh=00_AQKhseQG9agmrGIH63JW-mQXtHH6Bzrx1-7KA2XzK-U2NA&oe=6AC752F4')
 
 
 
